98f93dc9bfe8560b7054c96a580e092462018615
mnl89IT <gajo.manuel@gmail.com>
2016-02-05 21:24:52 +0100

    first commit

---
+++ app/main-page.js
@@ -0,0 +1,19 @@
+var observableModule = require("data/observable");
+var ObservableEventData = require("data/observable");
+
+var page;
+
+var pageData = new observableModule.Observable({
+    ss: { // switchers status
+        's1': false,
+        's2': true,
+        's3': false,
+        's4': true
+    }
+});
+
+exports.loaded = function(eventData) {
+    // binding context
+    page = eventData.object;
+    page.bindingContext = pageData;
+};
