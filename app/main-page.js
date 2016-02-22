a0b37b76d4b4740e5a09ebda9c3171391f712abe
Manuel <dragGH102@users.noreply.github.com>
2016-02-22 13:24:02 +0100

    fix switchers binding and refactor layout

--- app/main-page.js
+++ app/main-page.js
@@ -3,13 +3,11 @@ var ObservableEventData = require("data/observable");
 
 var page;
 
-var pageData = new observableModule.Observable({
-    ss: { // switchers status
-        's1': false,
-        's2': true,
-        's3': false,
-        's4': true
-    }
+var pageData = new observableModule.Observable({// switchers status
+    's1': false,
+    's2': true,
+    's3': false,
+    's4': true
 });
 
 exports.loaded = function(eventData) {
